f2f10d06bdc60ad3bf28aecc5f786f46cdf0975d
miloCODE <juamcamirodri@gmail.com>
2024-05-17 16:04:19 -0500

    test done

--- Estructurales/decorator/src/main/java/com/decorator/Main.java
+++ Estructurales/decorator/src/main/java/com/decorator/Main.java
@@ -2,6 +2,6 @@ package com.decorator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Funcionan los tests, ejecuta 'mvn test' para comprobarlo.");
     }
 }
